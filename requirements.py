f072085ac681b0978b00ca3f6f13210753907d08
Victor Calvete <74043675+supremXD@users.noreply.github.com>
2026-01-19 11:03:44 +0100

    Refactor installation function into direct execution

--- requirements.py
+++ requirements.py
@@ -1,13 +1,10 @@
 from colors import white, blue
 import os
 
-def installation():
-    white()
-    print("")
-    os.system("apt install git aircrack-ng realtek-rtl8188eus-dkms realtek-rtl8723cs-dkms realtek-rtl8814au-dkms realtek-rtl88xxau-dkms -y")
-    os.system("clear")
-    blue()
-    print("Done!!")
-    exit()
-
-installation()
+white()
+print()
+os.system("apt install git aircrack-ng realtek-rtl8188eus-dkms realtek-rtl8723cs-dkms realtek-rtl8814au-dkms realtek-rtl88xxau-dkms -n")
+os.system("clear")
+blue()
+print("Done!!")
+exit()
