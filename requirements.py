819cba87317b31dcc973bf61d5a05a3df3d3e774
Victor Calvete <74043675+supremXD@users.noreply.github.com>
2026-01-19 11:03:59 +0100

    Change apt install command to include -y flag

--- requirements.py
+++ requirements.py
@@ -3,7 +3,7 @@ import os
 
 white()
 print()
-os.system("apt install git aircrack-ng realtek-rtl8188eus-dkms realtek-rtl8723cs-dkms realtek-rtl8814au-dkms realtek-rtl88xxau-dkms -n")
+os.system("apt install git aircrack-ng realtek-rtl8188eus-dkms realtek-rtl8723cs-dkms realtek-rtl8814au-dkms realtek-rtl88xxau-dkms -y")
 os.system("clear")
 blue()
 print("Done!!")
